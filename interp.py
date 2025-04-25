da86cb494e00e2ac94f7142be7f939b64496d4ce
ethan dibble <edibble@pdx.edu>
2025-04-25 02:32:52 -0700

    tune join

--- interp.py
+++ interp.py
@@ -1,13 +1,60 @@
+#!/usr/bin/env python
+
+# NOTE: midiutil is included as suggested in the project information
+from midiutil import MIDIFile # version 1.2.1
+
 from dataclasses import dataclass
 from typing import Any
 
-type Literal = int | bool
+# DOMAIN SPECIFIC TYPES
+# -----------------------------------------------
+type Pitch = str    # keys on the piano
+type Duration = int # in seconds
+type Note_t = tuple[Pitch, Duration]
+type Tune_t = tuple[Note_t, ...]
+emptyTune: Tune_t = ()
+
+# map notes to midi numbers on octave 3
+# https://computermusicresource.com/midikeys.html
+DEGREES = {
+    "C": 60,
+    "D": 62,
+    "E": 64,
+    "F": 65,
+    "G": 67,
+    "A": 69,
+    "B": 71,
+    "R": None, # Rest
+}
+
+def extendTune(pitch: Pitch, duration: Duration, tune: Tune_t) -> Tune_t:
+    return tune + ((pitch, duration),)
+# -----------------------------------------------
+
+type Literal = int | bool | Tune
 type Expr = Lit | Add | Sub | Mul | Div | Neg | And | Or | Not | Eq | Neq | \
-            Lt | Gt | Leq | Geq | If | Let | Name
+            Lt | Gt | Leq | Geq | If | Let | Name | Join
 
 type Binding[V] = tuple[str,V]  # this tuple type is always a pair
 type Env[V] = tuple[Binding[V], ...] # this tuple type has arbitrary length
 emptyEnv : Env[Any] = ()  # the empty environment has no bindings
+
+# DOMAIN SPECIFIC EXTENSION
+@dataclass
+class Tune:
+    """{Str, Int} Note"""
+    tune: Tune_t
+    def __str__(self) -> str:
+        return ",".join(f"({p}, {d})" for (p, d) in self.tune)
+
+# DOMAIN SPECIFIC EXTENSION
+@dataclass
+class Join:
+    """{Str, Int} Note"""
+    left: Expr
+    right: Expr
+    def __str__(self) -> str:
+        return f"({self.left} | {self.right})"
 
 @dataclass
 class Lit:
@@ -156,6 +203,17 @@ def isInt(*args):
 def isBool(*args):
     return all(type(x) is bool for x in args)
 
+def isTune(*args):
+    return all
+
+def lookupEnv[V](name: str, env: Env[V]) -> V :
+    '''Return the first value bound to name in the input environment env
+       (or raise an exception if there is no such binding)'''
+    try:
+        return next(v for (n,v) in env if n == name)
+    except StopIteration:
+        raise EnvError('name is not in environment: ' + name)
+
 class EvalError(Exception):
     """Invalid Expressions"""
     pass
@@ -168,7 +226,7 @@ def extendEnv[V](name: str, value: V, env:Env[V]) -> Env[V]:
 def eval(e: Expr) -> Literal :
     return evalInEnv(emptyEnv, e)
 
-def evalInEnv(env: Env[bool], e:Expr) -> bool:
+def evalInEnv(env: Env[Literal], e:Expr) -> bool:
     match e:
         case Lit(lit):
             return lit
@@ -177,28 +235,28 @@ def evalInEnv(env: Env[bool], e:Expr) -> bool:
         # --------------------
 
         case Add(l, r):
-            match (eval(l), eval(r)):
+            match (evalInEnv(env, l), evalInEnv(env, r)):
                 case (l, r) if isInt(l, r):
                     return l + r
                 case _:
                     raise EvalError("addition of non-integers")
 
         case Sub(l, r):
-            match (eval(l), eval(r)):
+            match (evalInEnv(env, l), evalInEnv(env, r)):
                 case (l, r) if isInt(l, r):
                     return l - r
                 case _:
                     raise EvalError("subtraction of non-integers")
 
         case Mul(l, r):
-            match (eval(l), eval(r)):
+            match (evalInEnv(env, l), evalInEnv(env, r)):
                 case (l, r) if isInt(l, r):
                     return l * r
                 case _:
                     raise EvalError("multiplication of non-integers")
 
         case Div(l, r):
-            match (eval(l), eval(r)):
+            match (evalInEnv(env, l), evalInEnv(env, r)):
                 case (l, r) if isInt(l, r):
                     if r == 0:
                         raise EvalError("division by zero")
@@ -207,7 +265,7 @@ def evalInEnv(env: Env[bool], e:Expr) -> bool:
                     raise EvalError("division of non-integers")
 
         case Neg(subexpr):
-            match eval(subexpr):
+            match evalInEnv(env, subexpr):
                 case subexpr if isInt(subexpr):
                     return -subexpr
                 case _:
@@ -219,33 +277,33 @@ def evalInEnv(env: Env[bool], e:Expr) -> bool:
         # EVEN IF the right operand is non-boolean based on test file
 
         case And(l, r):
-            match eval(l):
+            match evalInEnv(env, l):
                 case l if isBool(l):
                     if not l:
                         return l
-                    match eval(r):
+                    match evalInEnv(env, r):
                         case r if isBool(r):
                             return r
-                        case _ :
+                        case _:
                             raise EvalError("R logical operation on non-boolean")
-                case _ :
+                case _:
                     raise EvalError("L logical operation on non-boolean")
 
         case Or(l, r):
-            match eval(l):
+            match evalInEnv(env, l):
                 case l if isBool(l):
                     if l:
                         return l
-                    match eval(r):
+                    match evalInEnv(env, r):
                         case r if isBool(r):
                             return r
-                        case _ :
+                        case _:
                             raise EvalError("R logical operation on non-boolean")
-                case _ :
+                case _:
                     raise EvalError("L logical operation on non-boolean")
 
         case Not(subexpr):
-            match eval(subexpr):
+            match evalInEnv(env, subexpr):
                 case subexpr if isBool(subexpr):
                     return not subexpr
                 case _:
@@ -255,13 +313,13 @@ def evalInEnv(env: Env[bool], e:Expr) -> bool:
         # ------------------
 
         case Eq(l, r):
-            (l, r) = (eval(l), eval(r))
+            (l, r) = (evalInEnv(env, l), evalInEnv(env, r))
             if type(l) != type(r):
                 return False
             return l == r
 
         case Neq(l, r):
-            (l, r) = (eval(l), eval(r))
+            (l, r) = (evalInEnv(env, l), evalInEnv(env, r))
             if type(l) != type(r):
                 return False
             return l != r
@@ -270,28 +328,28 @@ def evalInEnv(env: Env[bool], e:Expr) -> bool:
         # --------------------
 
         case Lt(l, r):
-            match (eval(l), eval(r)):
+            match (evalInEnv(env, l), evalInEnv(env, r)):
                 case (l, r) if isInt(l, r):
                     return l < r
                 case _:
                     raise EvalError("relational operation on non-integer")
 
         case Gt(l, r):
-            match (eval(l), eval(r)):
+            match (evalInEnv(env, l), evalInEnv(env, r)):
                 case (l, r) if isInt(l, r):
                     return l > r
                 case _:
                     raise EvalError("relational operation on non-integer")
 
         case Leq(l, r):
-            match (eval(l), eval(r)):
+            match (evalInEnv(env, l), evalInEnv(env, r)):
                 case (l, r) if isInt(l, r):
                     return l <= r
                 case _:
                     raise EvalError("relational operation on non-integer")
 
         case Geq(l, r):
-            match (eval(l), eval(r)):
+            match (evalInEnv(env, l), evalInEnv(env, r)):
                 case (l, r) if isInt(l, r):
                     return l >= r
                 case _:
@@ -301,13 +359,13 @@ def evalInEnv(env: Env[bool], e:Expr) -> bool:
         # ----------------------
 
         case If(cond, thenexpr, elseexpr):
-            match eval(cond):
+            match evalInEnv(env, cond):
                 case cond if isBool(cond):
                     if cond:
-                        return eval(thenexpr)
+                        return evalInEnv(env, thenexpr)
                     else:
-                        return eval(elseexpr)
-                case _ :
+                        return evalInEnv(env, elseexpr)
+                case _:
                     raise EvalError("if condition must be a boolean")
 
         # Let Bindings
@@ -317,9 +375,20 @@ def evalInEnv(env: Env[bool], e:Expr) -> bool:
              return lookupEnv(n,env)
 
         case Let(n,d,b):
-          v = evalInEnv(env, d)
-          newEnv = extendEnv(n, v, env)
-          return evalInEnv(newEnv, b)
+            v = evalInEnv(env, d)
+            newEnv = extendEnv(n, v, env)
+            return evalInEnv(newEnv, b)
+
+        # Join (represented by '|')
+        # -------------------------
+
+        case Join(l, r):
+            # DOMAIN SPECIFIC EXTENSION
+            match (evalInEnv(env, l), evalInEnv(env, r)):
+                case (Tune(l), Tune(r)):
+                    return l + r
+                case _:
+                    raise EvalError("non-joinable type")
 
 def run(e: Expr):
     print(f"running {e}")
@@ -328,4 +397,7 @@ def run(e: Expr):
     except EvalError as err:
         print(err)
 
-print(type(0), type(False))
+if __name__ == "__main__":
+    # Lit(Tune((("A", 1)))) has a lot of parens so be careful
+    run(Lit(Tune((("A", 1),))))
+    run(Join(Lit(Tune((("A", 1),))), Lit(Tune((("B", 2),)))))
